319f5fe6731b865ed159b19944f4fe53cfa6d762
zakariabanid101 <banidzakaria2@gmail.com>
2024-12-11 18:11:50 +0100

    create employee service

--- src/main/java/com/employee/backend/repositoties/EmployeeRepository.java
+++ src/main/java/com/employee/backend/repositoties/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.employee.backend.repositoties;
 
 import com.employee.backend.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
---
+++ src/main/java/com/employee/backend/services/EmployeeServices.java
@@ -0,0 +1,36 @@
+package com.employee.backend.services;
+
+import com.employee.backend.entities.Employee;
+import com.employee.backend.repositoties.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServices {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(int id) {
+
+        return employeeRepository.findById(id).get();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
