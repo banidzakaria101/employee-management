33c5d741b1dcde53bcaadf49fc9bd3a3a2aa0f97
zakariabanid101 <banidzakaria2@gmail.com>
2024-12-11 17:41:28 +0100

    add employee entity

---
+++ src/main/java/com/employee/backend/entities/Employee.java
@@ -0,0 +1,28 @@
+package com.employee.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+}
