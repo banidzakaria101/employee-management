74805ecd1e66b15fab6df5dc578b753c1bd1340e
zakariabanid101 <banidzakaria2@gmail.com>
2024-12-11 17:36:52 +0100

    initialize project

---
+++ src/main/java/com/employee/backend/EmployeeManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.employee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementBackEndApplication.class, args);
+	}
+
+}
