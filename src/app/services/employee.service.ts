b6ba13fe7104e503ebe73f13b9970012203d3edc
zakariabanid101 <banidzakaria2@gmail.com>
2024-12-12 01:50:08 +0100

    add all methodes needed to employee service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -14,4 +14,16 @@ export class EmployeeService {
   addEmployee(employee:Employee): Observable<Employee> {
     return this.http.post<Employee>(`${this.baseUrl}/add`, employee);
   }
+
+  getAllEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrl}/all`);
+  }
+
+  updateEmployee(id: number, employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.baseUrl}/update/${id}`, employee);
+  }
+
+  deleteEùployee(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/delete/${id}`);
+  }
 }
