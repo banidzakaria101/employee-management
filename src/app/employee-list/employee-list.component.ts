6789d81eb6d700b53abb0c8519160c3e51fdac81
zakariabanid101 <banidzakaria2@gmail.com>
2024-12-12 13:18:08 +0100

    add list-employee component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../model/employee.model';
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit{
+  employees: Employee[] = [];
+  employeeDialog: boolean = false;
+  currentEmployee: Employee = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
+  isEditing: boolean = false;
 
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees(){
+    this.employeeService.getAllEmployees().subscribe({
+      next: (data) => this.employees = data,
+      error: (err) => console.error('Error loading employees', err)
+    });
+  }
+
+  openNewEmployeeDialog() {
+    this.currentEmployee = {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+    this.isEditing = false;
+    this.employeeDialog = true;
+  }
+
+  editEmployee() {
+    if(this.isEditing && this.currentEmployee.id) {
+      this.employeeService.updateEmployee(this.currentEmployee.id, this.currentEmployee).subscribe({
+        next: () => {
+          this.loadEmployees();
+          this.employeeDialog = false;
+        },
+        error: (err) => console.error('Error updating employee', err)
+      });
+    }else {
+      this.employeeService.addEmployee(this.currentEmployee).subscribe({
+        next: () => {
+          this.loadEmployees();
+          this.employeeDialog = false;
+        },
+        error: (err) => console.error('Error adding employee', err)
+      });
+    }
+  }
+
+  isFomValid(): boolean {
+    return !!(
+      this.currentEmployee.firstName &&
+      this.currentEmployee.lastName &&
+      this.currentEmployee.email
+    );
+  }
 }
